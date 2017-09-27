52675a14f1fbd7bcdf841ea6c7df86c88ba04873
Joshua M. Bedilion <jbedilio@yahoo.com>
2017-09-26 23:47:12 -0400

    building the constructor for the basic Genie

--- basicCard.js
+++ basicCard.js
@@ -1,6 +1,21 @@
-function Genie(name, front, back) {
+//setting the constructor 
+function BasicGenie(name, front, back) {
+
+    //making the constructor scope safe
     if (this instanceof Genie){
+
+        //the name of the card (i.e. first president)
         this.name = name;
+
+        //the front of the card to hold the question (i.e. "Who was the first president of the US?")
         this.front = front;
+        
+        //the back of the card to hold the answer (i.e. George Washington)
         this.back = back;
+
+    }else{
+
+        //building the constructor to work without the 'new' keyword
+        return new BasicGenie(name, front, back);
+    }
 }

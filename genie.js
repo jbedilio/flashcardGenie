b2cde81ca24cb9f58a5cac66c1812f82d0669a1f
Joshua M. Bedilion <jbedilio@yahoo.com>
2017-09-27 09:48:49 -0400

    building the inquire promt and response

--- genie.js
+++ genie.js
@@ -2,9 +2,14 @@ var inquirer = require('inquirer');
 
 var basicGenie = require('./basicCard.js');
 
-
+var george = BasicGenie('first president', 'Who was the first president of the United States', 'George Washington');
 
 inquirer.prompt([
-    var george = BasicGenie(/* Pass your questions in here */]).then(function (answers) {
-    // Use user feedback for... whatever!!
+    {   name: 'front',
+        message: 'Enter your question fool!'
+    },
+    {   name: 'back',
+        message: 'Enter your answer fool!'}
+                 /* Pass your questions in here */]).then(function (card) {
+        var card = BasicGenie(name, front, back);
 });
